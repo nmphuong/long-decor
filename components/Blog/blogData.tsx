c7aa374574ce74c8a9068946014db5964fef69f0
Phuong Nguyen <dev.richard.npm98@gmail.com>
2023-05-08 14:00:03 +0700

    udpate code

---
+++ components/Blog/blogData.tsx
@@ -0,0 +1,61 @@
+import { Blog } from "@/types/blog";
+
+const blogData: Blog[] = [
+  {
+    id: 1,
+    title: "Decor phòng tắm cho nữ nhìn là thích",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-japan.jpg",
+    author: {
+      name: "Samuyl Joshi",
+      image: "/images/blog/author-01.png",
+      designation: "Graphic Designer",
+    },
+    tags: ["Japan"],
+    publishDate: "2025",
+  },
+  {
+    id: 2,
+    title: "Decor phòng tắm cho nữ nhìn là thích",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-japan.jpg",
+    author: {
+      name: "Musharof Chy",
+      image: "/images/blog/author-02.png",
+      designation: "Content Writer",
+    },
+    tags: ["Japan"],
+    publishDate: "2025",
+  },
+  {
+    id: 3,
+    title: "Decor phòng tắm cho nữ nhìn là thích",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-japan.jpg",
+    author: {
+      name: "Lethium Deo",
+      image: "/images/blog/author-03.png",
+      designation: "Graphic Designer",
+    },
+    tags: ["Japan"],
+    publishDate: "2025",
+  },
+  {
+    id: 4,
+    title: "Decor phòng tắm cho nữ nhìn là thích",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    image: "/images/blog/blog-japan.jpg",
+    author: {
+      name: "Lethium Deo",
+      image: "/images/blog/author-03.png",
+      designation: "Graphic Designer",
+    },
+    tags: ["Japan"],
+    publishDate: "2025",
+  },
+];
+export default blogData;
